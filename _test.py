f54d62d7c3d0b05ec13dd5c679938ec017877749
Will Poulett <william.poulett1@nhs.net>
2023-11-07 09:09:12 +0000

    two tests

--- _test.py
+++ _test.py
@@ -2,7 +2,7 @@ import pytest
 import pandas as pd
 import numpy as np
 
-from functions import create_mixup_images
+from functions import create_mixup_images, find_boundary_points
 
 def test_mixup():
 
@@ -37,3 +37,28 @@ def test_mixup():
 
 
 def test_boundary_points():
+    MUBA_ITERS = 3
+
+    muba_df = pd.DataFrame(
+        [[0.1,0.9,np.array([1]),np.array([0]),np.array([0.1]),"mix",1,0],
+         [0.4,0.6,np.array([1]),np.array([0]),np.array([0.4]),"mix",1,0],
+         [0.8,0.2,np.array([1]),np.array([0]),np.array([0.8]),"mix",1,1],#pred change
+         [0.2,0.8,np.array([1]),np.array([0]),np.array([0.2]),"mix",1,0],
+         [0.5,0.5,np.array([1]),np.array([0]),np.array([0.5]),"mix",1,1],#pred change
+         [0.9,0.1,np.array([1]),np.array([0]),np.array([0.9]),"mix",1,0] #pred change
+         ],
+         columns = ["alpha_class_0","alpha_class_1","image_0","image_1","mixup_image","type","label","argmax_pred"]
+    )
+
+    boundary_df = find_boundary_points(muba_df,MUBA_ITERS)
+
+    expected_df = pd.DataFrame(
+        [[0.6,0.4,np.array([1]),np.array([0]),np.array([0.6]),"boundary",0],
+         [0.35,0.65,np.array([1]),np.array([0]),np.array([0.35]),"boundary",1],
+         [0.7,0.3,np.array([1]),np.array([0]),np.array([0.7]),"boundary",0],
+         ],
+         columns = ["alpha_class_0","alpha_class_1","image_0","image_1","mixup_image","type","label"]
+    )
+
+    pd.testing.assert_frame_equal(boundary_df,expected_df,check_dtype=False)
+
